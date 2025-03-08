01013fe0b2ddcadb76a56b565bbe12001fd15880
JT3219 <justin.b.thompkins@gmail.com>
2025-03-07 19:22:53 -0600

    Added backend modules for User Profile and Login functionality with test files

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,28 +1,28 @@
-require('dotenv').config();
-
-
 const express = require('express');
 const cors = require('cors');
 
 const volunteerHistoryRouter = require('./volunteerHistoryRoutes');
-const notificationRouter = require('./notificationRoutes'); 
+const notificationRouter = require('./notificationRoutes');
+const loginRouter = require('./loginRoutes');
+const userProfileRouter = require('./userProfileRoutes');
 const volunteerSearchRouter = require('./volunteerSearchRoutes');
 const eventRouter = require('./eventManagementRoutes');
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
+// API Routes
 app.use('/api/volunteer-history', volunteerHistoryRouter);
-
 app.use('/api/notifications', notificationRouter);
-
+app.use('/api/login', loginRouter);
+app.use('/api/user-profiles', userProfileRouter);
 app.use('/api/match-volunteers', volunteerSearchRouter);
 app.use('/api/event-management', eventRouter);
 
-const PORT = process.env.PORT || 3000;
+// Setting the server port
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
